515ff2b5e779966baeb771b6fadf8ffdf5f6c94b
HafsaR <hafsa34343@gmail.com>
2022-11-29 23:46:31 -0600

    RTL

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,15 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
-test("Greet render correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
 
-test("Green render with the name", () => {
-let name = 'Raees'
-  render(<Greet name={name} />);
-  const textElement = screen.getByText(`Hello ${name}`);
-  expect(textElement).toBeInTheDocument();
+  test("render correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("render with the name", () => {
+    let name = "Raees";
+    render(<Greet name={name} />);
+    const textElement = screen.getByText(`Hello ${name}`);
+    expect(textElement).toBeInTheDocument();
+  });
+
 });
